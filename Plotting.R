3f2c49b50078c326e60234a7ee94b6021872973f
jluthy <jluthy123@gmail.com>
2019-08-28 23:16:37 -0700

    updated

--- Plotting.R
+++ Plotting.R
@@ -1,5 +1,6 @@
 #Load Libs. Will be using  data provided from the package ggplot2
 library(ggplot2)
+library(rayshader) # For 3D plotting
 
 qplot(displ,hwy,data=mpg)
 qplot(displ,hwy,data=mpg,color=drv)
@@ -11,7 +12,8 @@ box_plot <- ggplot(iris, aes(x=Species, y=Sepal.Width, fill=Species)) +
 geom_boxplot() +
 labs(x="Species", y="Sepal Width (cm)", fill="Species", title="Iris Dataset")
 box_plot
-# Can I make this a 3D plot?
+
+# Can I make this a 3D plot? No...
 plot_gg(box_plot,multicore = TRUE, width = 8, height = 8, scale = 300,
         zoom = 0.6, phi = 60, background = "#afceff",
         shadowcolor = "#3a4f70")
@@ -19,7 +21,7 @@ plot_gg(box_plot,multicore = TRUE, width = 8, height = 8, scale = 300,
 # Histogram with Auto Binning
 g <- ggplot(cannabinoids, aes(CBD)) + scale_fill_brewer(palette = "Spectral")
 
-g + geom_histogram(aes(fill=Sample ID),
+g + geom_histogram(aes(fill=Sample_ID),
                    binwidth = .1,
                    col="black",
                    size=.1) +  # change binwidth
@@ -32,15 +34,10 @@ g + geom_histogram(aes(fill=Sample ID),
 theme_set(theme_classic())
 # Histogram on a Categorical variable
 g <- ggplot(mpg, aes(manufacturer)) +
-  geom_bar(aes(fill=class), width = 0.5) +
-  theme(axis.text.x = element_text(angle=65, vjust=0.6)) +
-  labs(title="Histogram on Categorical Variable",
-       subtitle="Manufacturer across Vehicle Classes")
-plot_gg(g, width = 4, height = 4, height_aes = mpg, zoom = 0.6)
-# g + geom_bar(aes(fill=class), width = 0.5) +
-#   theme(axis.text.x = element_text(angle=65, vjust=0.6)) +
-#   labs(title="Histogram on Categorical Variable",
-#        subtitle="Manufacturer across Vehicle Classes")
+  g + geom_bar(aes(fill = class), width = 0.5) +
+  theme(axis.text.x = element_text(angle = 65, vjust = 0.6)) +
+  labs(title = "Histogram on Categorical Variable",
+       subtitle = "Manufacturer across Vehicle Classes")
 
 
 # ggplot object
